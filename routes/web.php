5afe40b8fc2b3df6e52ec4abff41567c7929e847
Filip Ostojic <dev.filip.ostojic@gmail.com>
2024-03-06 12:16:19 +0100

    changes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 use Illuminate\Support\Facades\File;
-
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 
 /*
@@ -18,16 +18,25 @@ use Illuminate\Support\Facades\File;
 */
 
 Route::get('/', function () {
-   $document= YamlFrontMatter::parse(
-      resource_path('posts/my-fourth-post.html')
-    );
-   ddd($document);
+
+    $files = File::files(resource_path("posts"));
+    $posts=[];
+
+    foreach ($files as $file){
+        $document = YamlFrontMatter::parseFile($file);
+        $posts[] = new Post(
+            $document->title,
+            $document->excerpt,
+            $document->date,
+            $document->body(),
+            $document->slug
+        );
+    }
 
 
-/*    return view('posts',[
-        'posts' => Post::all()
+   return view('posts',[
+        'posts' => $posts
     ]);
-*/
 });
 
 Route::get('posts/{post}', function ($slug) {
